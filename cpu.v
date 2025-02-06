5d1c7d34a59b1166099b468ff1604f2cf58a9302
João Victor de Melo Bezerra <joaovicmelobez@gmail.com>
2025-02-06 20:49:41 -0300

    feat: super early start of the CPU wires

---
+++ cpu.v
@@ -0,0 +1,16 @@
+module cpu(
+    input wire clk, reset
+);
+
+    //single bit wires
+    wire PCWrite;
+    wire PCWriteCond;
+    wire EPCWrite;
+    wire MultDiv;
+
+    // TO DO
+    // 1. Add the mux control signals
+    
+
+
+endmodule
