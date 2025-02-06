eebdb1a01aba753ca3f1228945325ce37e979b65
João Victor de Melo Bezerra <joaovicmelobez@gmail.com>
2025-02-06 15:08:57 -0300

    fix: adding a 8 bits extend

--- components/sign-extend/sgnExtnd16_32.v
+++
@@ -1,8 +0,0 @@
-module sgnExtnd1_32(
-    input wire [15:0] sign_extend,
-    output wire [31:0] out
-);
-
-    assign out =  (Data_in[15]) ? {{16{1'b1}}, Data_in} : {{16{1'b0}}, Data_in};;
-
-endmodule
---
+++ components/sign-extend/sgnExtnd8_16_32.v
@@ -0,0 +1,17 @@
+module sgnExtnd1_32(
+    input wire selector,
+    input wire [7:0] data_mdr,
+    input wire [15:0] data_instruction,
+    output wire [31:0] out
+);
+
+    wire [31:0] out_mdr, out_instruction
+
+    assign out_mdr =  (Data_in[15]) ? {{16{1'b1}}, Data_in} : {{16{1'b0}}, Data_in};;
+
+    assign out_instruction = {24'b0, data_instruction};
+
+    assign out = (selector == 1'b0) ? out_mdr : out_instruction;
+
+
+endmodule
