05fd14b25da47aa63f8777b59823409b84cc512b
João Victor de Melo Bezerra <joaovicmelobez@gmail.com>
2025-02-07 21:33:57 -0300

    feat: adding the control signals to cpu.v

--- cpu.v
+++ cpu.v
@@ -2,15 +2,55 @@ module cpu(
     input wire clk, reset
 );
 
-    //single bit wires
-    wire PCWrite;
-    wire PCWriteCond;
-    wire EPCWrite;
+    // mux control signals
+    wire PCWriteCondSource;
+    wire [1:0] IorD;
+    wire [1:0] ExCause;
+    wire MemA;
+    wire MemB;
     wire MultDiv;
+    wire [1:0] ALUSrcA;
+    wire [1:0] ALUSrcB;
+    wire [1:0] PCSrc;
+    wire [1:0] RegDst;
+    wire [2:0] WriteData;
+    wire ShiftIn;
+    wire ShiftS;
 
-    // TO DO
-    // 1. Add the mux control signals
-    
+    // register signals
+    wire PCWriteCond;
+    wire PCWrite;
+    wire [1:0] SCtrl;
+    wire MDRCtrl;
+    wire [1:0] LCtrl;
+    wire LoadAB;
+    wire ALUOut;
+    wire EPCWrite;
+    wire HiLow;
+    wire AuxMultDivA;
+    wire AuxMultDivB;
+
+    // memory signals
+    wire MemRead;
+    wire MemWrite;
+
+    // instruction register signals
+    wire IRWrite;
+
+    // register signals
+    wire RegWrite;
+
+    // ALUOP
+    wire [2:0] ALUOp;
+
+    //mult, div and shifting units
+    wire [2:0] ShiftCtrl;
+    wire MultCtrl;
+    wire DivCtrl;
+
+    // others
+    wire SignExtndCtrl;
+
 
 
 endmodule
