e370c6f19334f09bef8373cbdf080f2ae926ce5f
João Victor de Melo Bezerra <joaovicmelobez@gmail.com>
2025-02-10 23:15:57 -0300

    feat: adding more components in cpu.v

--- cpu.v
+++ cpu.v
@@ -61,12 +61,46 @@ module cpu(
     wire [4:0] instruction25_21;
     wire [4:0] instruction20_16;
     wire [15:0] instruction15_0;
-    
+
     wire [31:0] outMuxRegDst;
     wire [31:0] outMuxWriteData;
     wire [31:0] outDataA;
     wire [31:0] outDataB;
 
+    wire [31:0] outA;
+    wire [31:0] outB;
+
+    wire [31:0] outALUResult;
+
+    wire [31:0] outEPC;
+
+    wire [31:0] outALUOut;
+
+    wire [31:0] outMultDivA;
+    wire [31:0] outMultDivB;
+
+    wire [31:0] outHI;
+    wire [31:0] outLOW;
+
+    wire [31:0] outMDR;
+
+    wire [31:0] outLAux;
+
+    wire [31:0] outSAux;
+
+    wire [31:0] outMultDivA;
+    wire [31:0] outMultDivB;
+
+    wire [31:0] outPC;
+    wire [31:0] outMuxPCWrite;
+    wire [31:0] outMuxPCWriteCond;
+
+    wire [31:0] outMuxExCause;
+
+    wire [31:0] outShiftingUnit;
+
+    wire outALULT;
+    wire [31:0] outEx1to32;
 
     // COMPONENTES
     // Memoria Memory
@@ -97,6 +131,7 @@ module cpu(
         instruction15_0
     );
 
+    //Banco de Registradores
     Banco_Reg Registers(
         //signals
         clk,
@@ -110,6 +145,192 @@ module cpu(
         //outputs
         outDataA,
         outDataB
+    );
+
+    //Multiplexadores
+    mux_IorD muxIorD(
+        //signals
+        IorD,
+        //inputs
+        outPC,
+        outALUOut,
+        outALUResult,
+        outMuxExCause,
+        //outputs
+        outMuxIorD,
+    );
+
+    mux_RegDst muxRegDst(
+        //signals
+        RegDst,
+        //inputs
+        instruction20_16,
+        instruction15_0,
+        //outputs
+        outMuxRegDst,
+    );
+
+    mux_WriteData muxWriteData(
+        //signals
+        WriteData,
+        //inputs
+        outALUOut,
+        outEx1to32,
+        outLAux,
+        outHI,
+        outLOW,
+        outShiftingUnit
+        //outputs
+        outMuxWriteData,
+    );
+
+    //Unidades de Shift e Sign Extend
+    Extend_1to32 ex_1to32(
+        //input
+        outALULT,
+        //output
+        outEx1to32,
+    );
+
+    Shift_Left2 SL2(
+        //Input
+
+        //Output
+    )
+
+    //Registradores
+    assign writePC = (PCWrite || (PCWriteCond && outMuxPCWriteCond)); 
+
+    Registrador PC(
+        //signals
+        clk,
+        reset,
+        writePC,
+        //inputs
+        outMuxPCWrite,
+        //outputs
+        outPC,
+    );
+
+    Registrador A(
+        //signals
+        clk,
+        reset,
+        LoadAB,
+        //inputs
+        outDataA,
+        //outputs
+        outA
+    );
+
+    Registrador B(
+        //signals
+        clk,
+        reset,
+        LoadAB,
+        //inputs
+        outDataB,
+        //outputs
+        outB
+    );
+
+    Registrador EPC(
+        //signals
+        clk,
+        reset,
+        EPCWrite,
+        //inputs
+        outALUResult,
+        //outputs
+        outEPC
+    );
+
+    Registrador ALUOut(
+        //signals
+        clk,
+        reset,
+        ALUOut,
+        //inputs
+        outALUResult,
+        //outputs
+        outALUOut
+    );
+
+    Registrador HI(
+        //signals
+        clk,
+        reset,
+        HiLow,
+        //inputs
+        outMultDivA,
+        //outputs
+        outHI,
+    );
+
+    Registrador LOW(
+        //signals
+        clk,
+        reset,
+        HiLow,
+        //inputs
+        outMultDivB,
+        //outputs
+        outLOW,
+    ;)
+
+    Registrador MDR(
+        //signals
+        clk,
+        reset,
+        MDRCtrl,
+        //inputs
+        outMemory,
+        //outputs
+        outMDR,
+    );
+
+    Registrador LAux(
+        //signals
+        clk,
+        reset,
+        LCtrl,
+        //inputs
+        outMDR,
+        //outputs
+        outLAux,
+    );
+
+    Registrador SAux(
+        //signals
+        clk,
+        reset,
+        SCtrl,
+        //inputs
+        outMDR,
+        //outputs
+        outSAux,
+    );
+
+    Registrador MultDivA(
+        //signals
+        clk,
+        reset,
+        AuxMultDivA,
+        //inputs
+        outMemory,
+        //outputs
+        outMultDivA,
+    );
+
+    Registrador MultDivB(
+        //signals
+        clk,
+        reset,
+        AuxMultDivB,
+        //inputs
+        outMemory,
+        //outputs
+        outMultDivB,
     );
 
 endmodule
