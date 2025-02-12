cb8377b8b93190792ade526af1a09c214251e227
joyvixtor <joaovicmelobez@gmail.com>
2025-02-12 16:39:10 -0300

    saving development of the unit control

--- components/controlUnit.v
+++ components/controlUnit.v
@@ -47,7 +47,7 @@ module controlUnit(
 
     output reg SignExtndCtrl,
     output reg [1:0] SCtrl,
-    output reg [1:0] Lc
+    output reg [1:0] LCtrl
 );
 
     reg [5:0] STATE;
@@ -56,5 +56,52 @@ module controlUnit(
     //STATES
     parameter ST_ADD = 6'b000000, //0
     parameter ST_ADDI = 6'b000001, //1
+    parameter ST_COMMON = 6'b100010; //34
+
+    //FUNCTS
+    parameter ADD = 6'h20;
+    parameter AND = 6'h24;
+
+
+
+    always @(posedge clk) begin
+        if (reset) begin
+            //REGISTRADORES
+            PCWriteCond = 0;
+            PCWrite = 0;
+            MDRCtrl = 0;
+            LoadAB = 0;
+            ALUOut = 0;
+            EPCWrite = 0;
+            HiLow = 0;
+            AuxMultDivA = 0;
+            AuxMultDivB = 0;
+            MemWrite = 0;
+            MemRead = 0;
+            IRWrite = 0;
+            RegWrite = 1;
+            SCtrl = 0;
+            LCtrl = 0;
+        end
+
+        else if (overflow && STATE != ST_ADDIU && STATE != ST_AND) begin
+            
+        end
+
+        else if (divByZero) begin
+            
+        end
+
+        else begin
+            case(STATE)
+                ST_COMMON: begin
+                    //FETCH
+                    if(COUNTER <= 2) begin
+
+                    end
+                end
+            endcase
+        end
+    end
 
 endmodule
