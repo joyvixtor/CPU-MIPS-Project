d3cca6cd735209b5b6c8ece25fb082adb38d2a7d
João Victor de Melo Bezerra <joaovicmelobez@gmail.com>
2025-02-16 13:59:19 -0300

    new state to ST_LUI

--- components/controlUnit.v
+++ components/controlUnit.v
@@ -1894,7 +1894,8 @@ module controlUnit(
                 end
 
                 ST_SLT : begin
-                    divOP = 1'b0;
+                    if (COUNTER == 0) begin
+                        divOP = 1'b0;
                         multOP = 1'b0;
                         shiftCtrl = 3'b000;
                         ALUOP = 3'b111; //
@@ -1910,6 +1911,48 @@ module controlUnit(
                         ExCause = 2'b00;
                         ALUSrcA = 2'b01; //
                         ALUSrcB = 3'b000; //
+                        MemA = 1'b0;
+                        MemB = 1'b0;
+
+                        COUNTER = COUNTER + 1;
+                        STATE = ST_SLT;
+
+                        PCWriteCond = 1'b0;
+                        PCWrite = 1'b0;
+                        MDRCtrl = 1'b0;
+                        LoadAB = 1'b0;
+                        ALUOut = 1'b0;
+                        EPCWrite = 1'b0;
+                        HiLow = 1'b0;
+                        AuxMultDivA = 1'b0;
+                        AuxMultDivB = 1'b0;
+                        SCtrl = 2'b00;
+                        LCtrl = 2'b00;
+                        ASrc = 1'b0;
+
+                        MemReadWrite = 1'b0;
+                        IRWrite = 1'b0;
+                        RegWrite = 1'b1; //
+
+                        SignExtndCtrl = 1'b0;
+                    end
+                    else if (COUNTER == 1) begin
+                        divOP = 1'b0;
+                        multOP = 1'b0;
+                        shiftCtrl = 3'b000;
+                        ALUOP = 3'b000;
+
+                        WriteData = 3'b000;
+                        muxShiftS = 2'b00;
+                        muxShiftIn = 2'b00;
+                        RegDst = 2'b00;
+                        muxPCWriteCondSource = 1'b0;
+                        PCSrc = 2'b00;
+                        MultDiv = 1'b0;
+                        IorD = 2'b00;
+                        ExCause = 2'b00;
+                        ALUSrcA = 2'b00;
+                        ALUSrcB = 3'b000;
                         MemA = 1'b0;
                         MemB = 1'b0;
 
@@ -1931,9 +1974,10 @@ module controlUnit(
 
                         MemReadWrite = 1'b0;
                         IRWrite = 1'b0;
-                        RegWrite = 1'b1; //
+                        RegWrite = 1'b0;
 
                         SignExtndCtrl = 1'b0;
+                    end
                 end
                 //FORMATO I
                 ST_ADDI : begin
